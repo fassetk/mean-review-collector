9ad63257cc846fe5328f6cc13fd70b1f43a89de6
fassetk <fassetk@wwu.edu>
2021-06-04 19:31:56 -0700

    finished part 2 of tutorial

--- app.test.py
+++ app.test.py
@@ -1,6 +1,11 @@
+import os
 import unittest
+import tempfile
 from app import app
 class BasicTestCase(unittest.TestCase):
+    def test_database(self):
+        tester = os.path.exists("flaskr.db")
+        self.assertTrue(tester)
     def test_home(self):
         tester = app.test_client(self)
         pages = ['/', 'about', 'register', 'login']
@@ -13,3 +18,32 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 404)
 if __name__ == '__main__':
     unittest.main()
+class FlaskrTestCase(unittest.TestCase):
+  # set up a new temp database for per test
+  def setUp(self):
+    self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+    app.app.config['TESTING'] = True
+    self.app = app.app.test_client()
+    app.init_db()
+  # destroy the temp database for per test
+  def tearDown(self):
+    os.close(self.db_fd)
+    os.unlink(app.app.config['DATABASE'])
+  def register(self, username, password):
+    return self.app.post('/register', data=dict(
+      username=username,
+      password=password
+    ), follow_redirects=True)
+  # test messages
+  def test_register_messages(self):
+    """Test register messages using helper functions."""
+    rv = self.register(
+            "cat@123.com",
+            ""
+    )
+    assert b'Password is required' in rv.data
+    rv = self.register(
+            "",
+            "123"
+    )
+    assert b'Username is required' in rv.data
